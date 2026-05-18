93fd3a42b80368313f2b8b50797055ae3d91958e
Jakub Kaczor <j.kaczor06@gmail.com>
2026-05-18 13:31:43 +0200

    added enemy rotation interpolation

---
+++ Source/ActionCombat/Public/Characters/LookAtPlayerComponent.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "LookAtPlayerComponent.generated.h"
+
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class ACTIONCOMBAT_API ULookAtPlayerComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere)
+	float Speed{ 400.f };
+public:	
+	ULookAtPlayerComponent();
+
+protected:
+	virtual void BeginPlay() override;
+
+public:	
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
+		
+};
