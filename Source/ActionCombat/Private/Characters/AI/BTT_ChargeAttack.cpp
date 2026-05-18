f4a12d3b080b120295ff78195a997a03ee52e1da
Jakub Kaczor <j.kaczor06@gmail.com>
2026-05-18 18:57:40 +0200

    made enemy move when charging at player

--- Source/ActionCombat/Private/Characters/AI/BTT_ChargeAttack.cpp
+++ Source/ActionCombat/Private/Characters/AI/BTT_ChargeAttack.cpp
@@ -2,6 +2,25 @@
 #include "AIController.h"
 #include "GameFramework/Character.h"
 #include "Animations/BossAnimInstance.h"
+#include "BehaviorTree/BlackboardComponent.h"
+#include "Navigation/PathFollowingComponent.h"
+
+
+UBTT_ChargeAttack::UBTT_ChargeAttack()
+{
+	bNotifyTick = true;
+}
+
+void UBTT_ChargeAttack::TickTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory, float DeltaSeconds)
+{
+	bool bIsReadyToCharge = OwnerComp.GetBlackboardComponent()->GetValueAsBool("IsReadyToCharge");
+	
+	if (bIsReadyToCharge)
+	{
+		OwnerComp.GetBlackboardComponent()->SetValueAsBool("IsReadyToCharge", false);
+		ChargeAtPlayer();
+	}
+}
 
 EBTNodeResult::Type UBTT_ChargeAttack::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
 {
@@ -12,5 +31,20 @@ EBTNodeResult::Type UBTT_ChargeAttack::ExecuteTask(UBehaviorTreeComponent& Owner
 	
 	BossAnim->bIsCharging = true;
 	
+	OwnerComp.GetBlackboardComponent()->SetValueAsBool("IsReadyToCharge", false);
+	
 	return EBTNodeResult::InProgress;
+}
+
+void UBTT_ChargeAttack::ChargeAtPlayer()
+{
+	APawn* PlayerRef{ GetWorld()->GetFirstPlayerController()->GetPawn() };
+	FVector PlayerLocation{ PlayerRef->GetActorLocation() };
+	
+	FAIMoveRequest MoveRequest{ PlayerLocation };
+	MoveRequest.SetUsePathfinding(true);
+	MoveRequest.SetAcceptanceRadius(AcceptableRadius);
+	
+	ControllerRef->MoveTo(MoveRequest);
+	ControllerRef->SetFocus(PlayerRef);
 }
--- Source/ActionCombat/Public/Characters/AI/BTT_ChargeAttack.h
+++ Source/ActionCombat/Public/Characters/AI/BTT_ChargeAttack.h
@@ -15,6 +15,16 @@ class ACTIONCOMBAT_API UBTT_ChargeAttack : public UBTTaskNode
 	
 	class UBossAnimInstance* BossAnim;
 	
+	UPROPERTY(EditAnywhere)
+	float AcceptableRadius{ 100.f };
+	
+protected:
+	virtual void TickTask ( UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory, float DeltaSeconds) override;
+	
 public:
+	UBTT_ChargeAttack();
+	
 	virtual EBTNodeResult::Type ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) override;
+	
+	void ChargeAtPlayer();
 };
